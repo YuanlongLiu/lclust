3a0b2d7a0a32452ed726458450646c888449b849
Kirill Ponomarev <ponomkirill@gmail.com>
2015-06-05 14:09:57 +0300

    Hooray!

--- R/draw.R
+++ R/draw.R
@@ -10,7 +10,7 @@
 #' 
 #' @export 
 #' @examples 
-#' A <- Crude
+#' A <- data(crude)
 #' draw(A, lclust(A, 2))
 #' B <- matrix(c(0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0), 4, 4)
 #' groups <- list(c(1, 3), c(2, 4))
@@ -28,3 +28,14 @@ draw <- function(A = matrix(), groups = list(), colors = terrain.colors(length(g
               vertex.label.color = "black", main = main)
   legend(x = "bottomleft", legend = paste("N =", length(groups)), bty = "n")
 }
+#' Data on international trade
+#' 
+#' 24 selected representative countries exchanging crude materials
+#' 
+#' @format Non-symmetric bunary matrix, trade goes from rows to columns.
+#' @docType data
+#' @keywords datasets
+#' @name crude
+#' @usage data(crude)
+#' @source \url{http://moreno.ss.uci.edu/data.html#trade}
+"crude"
